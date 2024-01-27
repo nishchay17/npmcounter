8540e20567dcf9a64e7e723fab9e3ac939fa5f17
Nishchay <nishchay13971@gmail.com>
2024-01-28 03:04:27 +0530

    feat(count): updated counter section

--- components/count.tsx
+++ components/count.tsx
@@ -1,14 +1,46 @@
 "use client";
 
+import { CopyIcon } from "@radix-ui/react-icons";
+
 import { useAppContext } from "@/context/app-context";
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function Count({ count }: { count: number }) {
-  const {
-    state: { total, status },
-  } = useAppContext();
-  const toDisplay = status === "not-loaded" ? count : total;
+  const { state } = useAppContext();
+  const [copiedText, copy] = useCopyToClipboard();
+  const toDisplay = state.status === "not-loaded" ? count : state.total;
   return (
-    <p className="text-center">Total downloads: {toDisplay.toLocaleString()}</p>
+    <div className="text-center">
+      <p className="text-xl">
+        Total{" "}
+        <span className="underline underline-offset-2">{state.package}</span>{" "}
+        downloads
+      </p>
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger>
+            <div
+              onClick={() => copy(toDisplay.toString())}
+              className="mt-2 cursor-pointer flex items-center justify-center gap-2 text-[var(--hero-red)]"
+            >
+              <span className="text-4xl  font-medium">
+                {toDisplay.toLocaleString()}
+              </span>
+              <CopyIcon height={24} width={24} />
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Copy it!</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   );
 }
 
