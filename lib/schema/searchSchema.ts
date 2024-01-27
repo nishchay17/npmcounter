a907627397b274dd33670681ac52d02b2a697d36
Nishchay <nishchay13971@gmail.com>
2024-01-28 03:14:33 +0530

    fix(search schema): fixed upper case npm package name

--- lib/schema/searchSchema.ts
+++ lib/schema/searchSchema.ts
@@ -2,7 +2,12 @@
 
 import * as z from "zod";
 
-export const searchSchema = z.object({
-  package: z.string().min(1, "Please enter a package name").max(50),
-  range: z.string().min(2).max(50),
-});
+export const searchSchema = z
+  .object({
+    package: z.string().min(1, "Please enter a package name").max(50),
+    range: z.string().min(2).max(50),
+  })
+  .transform((data) => {
+    data.package = data.package?.toLowerCase();
+    return data;
+  });
