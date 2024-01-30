922aacdbec4e5b9de0a655ce0816b2ddb1f006d1
Nishchay <nishchay13971@gmail.com>
2024-01-31 00:55:45 +0530

    feat(landing): changed main card bg

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   return (
     <div className="px-3 md:px-4 md:container sm:space-y-3">
       <Nav />
-      <main className="border py-8 px-3 pb-3 md:py-10 lg:py-14 md:px-5 lg:px-9 rounded-2xl bg-primary-foreground space-y-8 sm:space-y-10 md:space-y-16">
+      <main className="border py-8 px-3 pb-3 md:py-10 lg:py-14 md:px-5 lg:px-9 rounded-2xl bg-background space-y-8 sm:space-y-10 md:space-y-16">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium text-center">
           Get count of npm <br /> packages quickly
         </h1>
